e2553d3613da56a57b5dd943a885dad4b9e58969
Whit Waldo <whit.waldo@innovian.net>
2023-05-04 05:35:43 -0500

    Bought up to standard format

---
+++ Interfaces/IHadId.cs
@@ -0,0 +1,6 @@
+﻿namespace Interfaces;
+
+public interface IHasId
+{
+    public Guid Id { get; }
+}
--- Models/Repository.cs
+++ Models/Repository.cs
@@ -1,56 +1,9 @@
-﻿using Metalama.Bits;
+﻿using Interfaces;
+using Metalama.Bits;
 
 namespace Models;
 
-public sealed class Repository<T> where T : IIdentifiable
+[RepositoryAspect]
+public class Repository<T> where T : IHasId
 {
-    public List<T> _entities = new();
-    //private readonly ILogger _logger;
-
-    //public Repository(ILogger<Repository<T>> logger)
-    public Repository()
-    {
-    }
-
-    [InjectedLogger]
-    public void Add(T data)
-    {
-        _entities.Add(data);
-    }
-
-
-    [InjectedLogger]
-    public void Remove(Guid id)
-    {
-        var remaining = _entities.Where(a => a.Id != id).ToList();
-        _entities = remaining;
-    }
-
-
-    [InjectedLogger]
-    public void Update(Guid id, [SensitiveData]T data)
-    {
-        for (var a = 0; a < _entities.Count; a++)
-        {
-            if (_entities[a].Id == id)
-            {
-                _entities[a] = data;
-            }
-        }
-    }
-
-
-    [InjectedLogger]
-    public List<T> List()
-    {
-        return _entities;
-    }
-
-
-    [InjectedLogger]
-    public T? Get(Guid id)
-    {
-        var entity = _entities.FirstOrDefault(a => a.Id == id);
-        return entity;
-    }
 }
