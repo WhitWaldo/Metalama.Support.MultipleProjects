04578f974322f6f12680ef48dfa18d59f5d0ebb7
Whit Waldo <whit.waldo@innovian.net>
2023-05-04 03:46:42 -0500

    Uncommented worker logic

--- Runner/Worker.cs
+++ Runner/Worker.cs
@@ -15,15 +15,15 @@ namespace Runner
 
         public void DoStuff([SensitiveData]string name)
         {
-            //_repository.Add(new Person("Nick"));
-            //_repository.Add(new Person("Bill"));
-            //var people = _repository.List();
-            //_repository.Remove(people[0].Id);
-            //_repository.Update(people[1].Id, new Person("Jill")
-            //{
-            //    Id = people[1].Id
-            //});
-            //var jill = _repository.Get(people[1].Id);
+            _repository.Add(new Person("Nick"));
+            _repository.Add(new Person("Bill"));
+            var people = _repository.List();
+            _repository.Remove(people[0].Id);
+            _repository.Update(people[1].Id, new Person("Jill")
+            {
+                Id = people[1].Id
+            });
+            var jill = _repository.Get(people[1].Id);
         }
     }
 }
